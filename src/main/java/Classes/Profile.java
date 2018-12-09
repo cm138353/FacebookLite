1fbe67d015a3da1c91e11b15a6e837651cacf8b1
cm138353 <christian.martinez442@yahoo.com>
2018-12-08 20:45:13 -0800

    deleted wall

--- src/main/java/Classes/Profile.java
+++ src/main/java/Classes/Profile.java
@@ -2,18 +2,7 @@ package Classes;
 
 //import sample.Friends;
 
-public class Profile
-{
-    private User user;  // declared
-    private Wall wall;
-    //private Friends friends;
-    
-    public Profile(String firstName , String lastName , int age)
-    {
-        //user = new User(firstName , lastName , age);  // initialized by constructor
-        //wall = new Wall();
-        //friends = new Friends();
-    }
-    
+public class Profile {
+
 
 }
