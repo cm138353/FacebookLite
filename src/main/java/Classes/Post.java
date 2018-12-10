5acf2e86925717ce8ba847d10633ced2e7368a54
Kamran B <29531233+ReJuggernaut@users.noreply.github.com>
2018-12-10 13:37:02 -0800

    postDao updates

--- src/main/java/Classes/Post.java
+++ src/main/java/Classes/Post.java
@@ -5,25 +5,27 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Post {
 
     private int id;
-    private String post;
+    private String content;
     private String dateAdded;
     private Boolean hidden;
 
     public Post(String post, String dateAdded, boolean hidden){
         this.id = ThreadLocalRandom.current().nextInt(1, 1000);
-        this.post = post;
+        this.content = post;
         this.dateAdded = dateAdded;
         this.hidden = hidden;
     }
 
     @Override
     public String toString(){
-        return ("Posted on " + this.dateAdded + ": \n" + this.post);
+        return ("Posted on " + this.dateAdded + ": \n" + this.content);
     }
 
     public int getID(){
         return this.id;
     }
+
+    public String getContent() { return  content;}
 
     public void setHidden(boolean hidden){
         this.hidden = hidden;
--- src/main/java/Daos/PostDao.java
+++ src/main/java/Daos/PostDao.java
@@ -2,11 +2,19 @@ package Daos;
 
 import Classes.Post;
 import Interface.Dao;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import sample.DbManager;
 
 import java.util.List;
 
 public class PostDao implements Dao<Post> {
+
+
+    MongoDatabase db = DbManager.getDb("FBDB");
+    MongoCollection postsCollection = db.getCollection("posts");
+
     @Override
     public List<Post> getAll() {
         return null;
@@ -20,7 +28,11 @@ public class PostDao implements Dao<Post> {
 
     @Override
     public void save(Post post) {
-
+        Document postDoc = new Document();
+        postDoc.put("credId", post.getID());
+        postDoc.put("content", post.getContent());
+        postDoc.put("isHidden", post.getHidden());
+        postsCollection.insertOne(postDoc);
     }
 
     @Override
