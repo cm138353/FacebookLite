fb06decf028cec334b5ba8ee04bb3e9dcc2abde8
Kamran B <29531233+ReJuggernaut@users.noreply.github.com>
2018-12-10 20:24:50 -0800

    user class updated and cleaned

--- src/main/java/Classes/User.java
+++ src/main/java/Classes/User.java
@@ -1,78 +1,22 @@
 package Classes;
 
-public class User
-{
+public class User {
     private String email;
     private String password;
-    private String fName;
-    private String lName;
-    private boolean hidden = false;
 
     public User(String e, String p) {
         this.email = e;
         this.password = p;
     }
 
-    public User(String fName, String lName, String e, String p){
-        this.email = e;
-        this.password = p;
-        this.lName = lName;
-        this.fName = fName;
-    }
 
-    public User(String fName, String lName, String e){
-        this.email = e;
-        this.lName = lName;
-        this.fName = fName;
-    }
-
-    public void setEmail(String email){
-
-        this.email = email;
-    }
-
-    public String getEmail(){
+    public String getEmail() {
 
         return email;
     }
 
-    public void setPassword(String password){
-
-        this.password = password;
-    }
-
-    public String getPassword(){
+    public String getPassword() {
 
         return password;
     }
-
-    public String getfName(){
-        return fName;
-    }
-
-    public void setfName(String fName){
-        this.fName = fName;
-    }
-
-    public String getlName() {
-        return lName;
-    }
-
-    public void setlName(String lName) {
-        this.lName = lName;
-    }
-
-    public void setHidden(boolean hidden) {
-        this.hidden = hidden;
-    }
-
-    public Boolean getHidden(){
-        return hidden;
-    }
-
-    @Override
-    public String toString(){
-        return (fName + " " + lName);
-    }
-
 }
